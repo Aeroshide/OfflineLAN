1fb564f0bf83825225b40a6f32dc2146dda3c410
Aeroshide <huskyisbadboi61@gmail.com>
2025-04-19 10:48:48 +0700

    first commit

---
+++ src/main/java/com/aerohide/offlinelan/Offlinelan.java
@@ -0,0 +1,10 @@
+package com.aerohide.offlinelan;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Offlinelan implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/aerohide/offlinelan/mixins/IntegratedServerMixin.java
@@ -0,0 +1,2 @@
+package com.aerohide.offlinelan.mixins;public class IntegratedServerMixin {
+}
