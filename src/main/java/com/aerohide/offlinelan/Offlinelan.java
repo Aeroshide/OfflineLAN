4e069301125c6b19f2ac8160b5e262787ef23099
Aeroshide <huskyisbadboi61@gmail.com>
2025-11-28 02:51:03 +0700

    build: changed java minimum target to 17

--- src/main/java/com/aerohide/offlinelan/Offlinelan.java
+++ src/main/java/com/aerohide/offlinelan/Offlinelan.java
@@ -1,9 +1,13 @@
 package com.aerohide.offlinelan;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.fabricmc.api.ModInitializer;
 
 public class Offlinelan implements ModInitializer {
     public static boolean onlineMode = true;
+    public static final Logger LOG = LogManager.getLogger("OfflineLAN");
     @Override
     public void onInitialize() {
     }
--- src/main/java/com/aerohide/offlinelan/mixins/IntegratedServerMixin.java
+++ src/main/java/com/aerohide/offlinelan/mixins/IntegratedServerMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,10 +14,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(IntegratedServer.class)
 public class IntegratedServerMixin {
-
-    @Inject(method = "openToLan", at = @At("HEAD"))
-    private void onOpenToLan(CallbackInfoReturnable<Boolean> cir) {
-        ((IntegratedServer)(Object)this).setOnlineMode(Offlinelan.onlineMode);
+    @Inject( method = "openToLan(Lnet/minecraft/world/GameMode;ZI)Z", at = @At("HEAD"))
+    private void onOpenToLan(GameMode gameMode, boolean cheatsAllowed, int port, CallbackInfoReturnable<Boolean> cir)
+    {
+        IntegratedServer integratedServer = (IntegratedServer) (Object) this;
+        integratedServer.setOnlineMode(Offlinelan.onlineMode);
+        // Offlinelan.LOG.info(integratedServer.isOnlineMode());
+        if (!integratedServer.isOnlineMode())
+            Offlinelan.LOG.info("The server will make no attempt to authenticate usernames. Beware.");
     }
 
 }
