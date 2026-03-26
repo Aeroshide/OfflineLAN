2e9360353564bf7e178079c89b969cf72d00b198
Aeroshide <huskyisbadboi61@gmail.com>
2026-03-26 18:29:22 +0700

    feat: 26.1 update

--- src/main/java/com/aerohide/offlinelan/mixins/IntegratedServerMixin.java
+++ src/main/java/com/aerohide/offlinelan/mixins/IntegratedServerMixin.java
@@ -1,12 +1,9 @@
 package com.aerohide.offlinelan.mixins;
 
 import com.aerohide.offlinelan.Offlinelan;
-import net.minecraft.server.integrated.IntegratedServer;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
+import net.minecraft.client.server.IntegratedServer;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.world.GameMode;
+import net.minecraft.world.level.GameType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,13 +11,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(IntegratedServer.class)
 public class IntegratedServerMixin {
-    @Inject( method = "openToLan(Lnet/minecraft/world/GameMode;ZI)Z", at = @At("HEAD"))
-    private void onOpenToLan(GameMode gameMode, boolean cheatsAllowed, int port, CallbackInfoReturnable<Boolean> cir)
+    @Inject( method = "publishServer(Lnet/minecraft/world/level/GameType;ZI)Z", at = @At("HEAD"))
+    private void onOpenToLan(GameType gameMode, boolean cheatsAllowed, int port, CallbackInfoReturnable<Boolean> cir)
     {
         IntegratedServer integratedServer = (IntegratedServer) (Object) this;
-        integratedServer.setOnlineMode(Offlinelan.onlineMode);
+        integratedServer.setUsesAuthentication(Offlinelan.onlineMode);
         // Offlinelan.LOG.info(integratedServer.isOnlineMode());
-        if (!integratedServer.isOnlineMode())
+        if (!integratedServer.usesAuthentication())
             Offlinelan.LOG.info("The server will make no attempt to authenticate usernames. Beware.");
     }
 
--- src/main/java/com/aerohide/offlinelan/mixins/OpenToLanScreenMixin.java
+++ src/main/java/com/aerohide/offlinelan/mixins/OpenToLanScreenMixin.java
@@ -1,39 +1,32 @@
 package com.aerohide.offlinelan.mixins;
 
 import com.aerohide.offlinelan.Offlinelan;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.Drawable;
-import net.minecraft.client.gui.Element;
-import net.minecraft.client.gui.Selectable;
-import net.minecraft.client.gui.screen.OpenToLanScreen;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.CyclingButtonWidget;
-import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.client.gui.components.CycleButton;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.ShareToLanScreen;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(OpenToLanScreen.class)
+@Mixin(ShareToLanScreen.class)
 public abstract class OpenToLanScreenMixin extends Screen {
-    protected OpenToLanScreenMixin(Text title) {
+    protected OpenToLanScreenMixin(Component title) {
         super(title);
     }
 
     @Inject(method = "init", at = @At("TAIL"))
     private void addOfflineModeToggle(CallbackInfo ci) {
         Offlinelan.onlineMode = true;
-        this.addDrawableChild(
-                CyclingButtonWidget.onOffBuilder(Offlinelan.onlineMode)
-                        .build(
-                                this.width / 2 + 5,
-                                130,
+        this.addRenderableWidget(
+                CycleButton.onOffBuilder(Offlinelan.onlineMode)
+                        .create(
+                                this.width / 2 - 75,
+                                190,
                                 150,
                                 20,
-                                Text.translatable("offlinelan.toggleText"),
+                                Component.translatable("offlinelan.toggleText"),
                                 (button, value) -> Offlinelan.onlineMode = value
                         )
         );
